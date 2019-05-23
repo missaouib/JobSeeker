8c1f3f8b8c6f0050896e4ad1e4abc3d5d92495b1
mokebe <mokebe>
2019-05-23 21:00:14 +0200

    fix: disable sorting names

--- Frontend/src/app/views/it-job-offers-in-world/it-job-offers-in-world.component.ts
+++ Frontend/src/app/views/it-job-offers-in-world/it-job-offers-in-world.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-it-job-offers-in-world',
   templateUrl: './it-job-offers-in-world.component.html',
   styleUrls: ['./it-job-offers-in-world.component.css']
 })
-export class ItJobOffersInWorldComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ItJobOffersInWorldComponent  {
 }
