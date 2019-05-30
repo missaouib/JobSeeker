44dd3e025653238e393d76c0c0aacd90bd325552
mokebe <mokebe>
2019-05-30 19:55:30 +0200

    feat: add favicon

--- Backend/src/main/java/com/Backend/BackendApplication.java
+++ Backend/src/main/java/com/Backend/BackendApplication.java
@@ -10,23 +10,24 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	//todo
-	//[REFACTOR] change input fields to autocomplete
-	//[FEAT] add switch language button to categories
-	//[FEAT] add other countries to technology view
-	//[REFACTOR] Consider init Categories by scraping request
-	//[REFACTOR] frontend architecture
-	//[REFACTOR} Think about how works particularly categories on selected sites
-	//[REFACTOR] Change to Polish city names on front-end site
-	//[FEAT] add RWD on all views
+	//[BACKEND] TODO
 	//[REFACTOR] stringbuilder
-	//[FEAT] add animations
-	//{REFACTOR] alphabetical cities
-    //[REFACTOR] simplify URLs and fix keywords
+	//[REFACTOR] Change to Polish city names on front-end site
+	//[REFACTOR] simplify URLs and fix keywords
 	//[REFACTOR] date to server side and other tables
 	//[FEAT] database, init methods
-	//[FEAT] add diagrams
 	//[FEAT] additional data {salary, experience, calculator}
 	//[FEAT] jwt python
 	//[TEST] add tests
+
+	//[FRONTEND] TODO
+	//[REFACTOR] change input fields to autocomplete /only statistics view
+	//[REFACTOR] change all tooltips
+	//{REFACTOR] alphabetical cities
+	//[FEAT] add RWD on all views
+	//[FEAT] add animations
+	//[FEAT] add slide toogle language button to categories
+	//[FEAT] add other countries to technology view
+	//[REFACTOR] shared input component
+	//[FEAT] add diagrams
 }
